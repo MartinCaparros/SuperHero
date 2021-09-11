8edc1100f984fb43355ade6d5f8195d564fe7f35
MartinCaparros <martincaparross@gmail.com>
2021-09-11 12:32:26 -0300

    SuperHero

---
+++ src/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CalculatorBody = styled.div `
+    border: 1px solid black;
+    height: 600px;
+    width: 300px;
+`;
+
+export const Container = styled.div `
+    display:flex;
+    justify-content:center;
+`;
